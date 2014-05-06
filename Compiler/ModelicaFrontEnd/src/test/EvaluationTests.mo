svn2github/JModelica_dot_org

commit cfb19c87ffeca0f636389c3f9b2a16e33cbb34b4

Fixed tests broken by r6266. (#3532)
git-svn-id: https://svn.jmodelica.org/trunk@6279 7ae51c65-13d5-4921-9d80-6511ddd1419c

Compiler/ModelicaFrontEnd/src/jastadd/ConstantEvaluation.jrag
@@ -1610,7 +1610,7 @@ aspect ConstantEvaluation {
         if (!var.inRecord())
             return false;
         if (!var.inFunction()) 
-            return var.hasParentRecordWithBindingExp();
+            return !var.hasBindingFExp() && var.hasParentRecordWithBindingExp();
         if (containingInstComponent() == var.containingInstComponent())
             return false;
         InstAccess first = getTopInstAccess().getFirstInstAccess();
@@ -2391,7 +2391,34 @@ aspect ConstantEvaluation {
     protected CValue InstRecord.cevalBindingExp() {
         if (hasBindingFExp())
             return myBindingInstExp().ceval();
-        CValueRecord res = new CValueRecord((FRecordType) type());
+        FRecordType type = (FRecordType) type().scalarType();
+        if (isArray()) {
+            CValueArray res = new CValueArray(size());
+            int ndims = ndims();
+            for (InstComponentDecl ch : getInstComponentDecls())
+                ch.cevalBindingExpForRecordArray(type, res, ndims);
+            return res;
+        } else {
+            return cevalBindingExpForRecord(type);
+        }
+    }
+
+    protected void InstComponentDecl.cevalBindingExpForRecordArray(FRecordType type, CValueArray arr, int ndims) {
+        throw new UnsupportedOperationException("Only valid for InstArrayComponentDecls.");
+    }
+
+    protected void InstArrayComponentDecl.cevalBindingExpForRecordArray(FRecordType type, CValueArray arr, int ndims) {
+        ndims--;
+        if (ndims > 0) {
+            for (InstComponentDecl ch : getInstComponentDecls())
+                ch.cevalBindingExpForRecordArray(type, arr, ndims);
+        } else {
+            arr.addCell(cevalBindingExpForRecord(type));
+        }
+    }
+
+    protected CValue InstComponentDecl.cevalBindingExpForRecord(FRecordType type) {
+        CValueRecord res = new CValueRecord(type);
         evaluationValue = res;
         for (InstComponentDecl ch : allInstComponentDecls())
             res.setMember(ch.name(), ch.cevalBindingExp());

Compiler/ModelicaFrontEnd/src/test/RecordTests.mo
@@ -1157,6 +1157,7 @@ end RecordTests.RecordArray6;
 end RecordArray6;
 
 
+// TODO: entire record is here turned into a structural parameter, but should only be n
 model RecordArray7
     record A
         parameter Integer n;
@@ -1164,7 +1165,7 @@ model RecordArray7
     end A;
     
     parameter Integer m = 2;
-    A a = A(m, 1:m);
+    parameter A a = A(m, 1:m);
     Real y[m] = a.x;
 
     annotation(__JModelica(UnitTesting(tests={
@@ -1174,8 +1175,8 @@ model RecordArray7
             flatModel="
 fclass RecordTests.RecordArray7
  parameter Integer m = 2 /* 2 */;
- RecordTests.RecordArray7.A a = RecordTests.RecordArray7.A(2, 1:2);
- Real y[2] = a.x[1:2];
+ parameter RecordTests.RecordArray7.A a = RecordTests.RecordArray7.A(2, 1:2) /* RecordTests.RecordArray7.A(2, { 1, 2 }) */;
+ Real y[2] = {1.0, 2.0};
 
 public
  record RecordTests.RecordArray7.A

commit 7760596a8f00eb2aa1e1b8548d4f678700533b61

Fixed evaluation of constants in records that was broken by r6279. (#3532)
git-svn-id: https://svn.jmodelica.org/trunk@6306 7ae51c65-13d5-4921-9d80-6511ddd1419c

Compiler/ModelicaFrontEnd/src/jastadd/ConstantEvaluation.jrag
@@ -1765,13 +1765,12 @@ aspect ConstantEvaluation {
         if (evaluationValue != null) {
             val = evaluationValue;
         } else if ((isConstant() || isParameter()) && !isForIndex()) {
-            boolean rec = inRecordDecl();
             boolean func = inFunction();
             if (hasBindingFExp()) {
                 FExp bexp = getBindingFExp();
-                if (!bexp.type().isUnknown() && !rec)
+                if (!bexp.type().isUnknown())
                     val = bexp.ceval(func ? i : Index.NULL);
-            } else if (!rec && !func) {
+            } else if (!func && !inRecordDecl()) {
                 val = startAttributeCValue();
             }
             if (!func)

Compiler/ModelicaFrontEnd/src/test/EvaluationTests.mo
@@ -1251,4 +1251,26 @@ end EvaluationTests.ParameterEvalAnnotation3;
 ")})));
 end ParameterEvalAnnotation3;
 
+
+model ConstantInRecord1
+    record A
+        constant Real a = 1;
+        constant Real b = a + 1;
+    end A;
+    
+    constant Real c = A.a;
+    constant Real d = A.b;
+
+    annotation(__JModelica(UnitTesting(tests={
+        FlatteningTestCase(
+            name="ConstantInRecord1",
+            description="Evaluation of constants in records",
+            flatModel="
+fclass EvaluationTests.ConstantInRecord1
+ constant Real c = 1.0;
+ constant Real d = 2.0;
+end EvaluationTests.ConstantInRecord1;
+")})));
+end ConstantInRecord1;
+
 end EvaluationTests;