svn2github/JModelica_dot_org

commit 354a27f7b3ffa95cfbce6c040e0524d0aa54e94f

Merge r6612 that was committed to branch by accident.
git-svn-id: https://svn.jmodelica.org/trunk@6655 7ae51c65-13d5-4921-9d80-6511ddd1419c

RuntimeLibrary/src/fmi1_me/fmi1_me.c
@@ -149,7 +149,6 @@ fmiComponent fmi1_me_instantiate_model(fmiString instanceName, fmiString GUID, f
     cb->allocate_memory = functions.allocateMemory;
     cb->free_memory = functions.freeMemory;
     cb->model_name = jmi_get_model_identifier();       /**< \brief Name of the model (corresponds to a fixed compiled unit name) */
-    cb->instance_name = instanceName;    /** < \brief Name of this model instance. */
     cb->model_data = component;
     
     resource_location = jmi_locate_resources(functions.allocateMemory);
@@ -167,7 +166,8 @@ fmiComponent fmi1_me_instantiate_model(fmiString instanceName, fmiString GUID, f
     tmpname = (char*)(fmi1_me_t *)functions.allocateMemory(inst_name_len, sizeof(char));
     strncpy(tmpname, instanceName, inst_name_len);
     component -> fmi_instance_name = tmpname;
-    
+    cb->instance_name = tmpname;  /** < \brief Name of this model instance. */
+
     return component;
 }
 

RuntimeLibrary/src/jmi/jmi_log.c
@@ -28,6 +28,8 @@
 /*#define INLINE inline */ /* not supported in c89 */
 #define INLINE 
 
+static void create_log_file_if_needed(log_t *log);
+
 const char* jmi_callback_log_category_to_string(jmi_log_category_t c) {
     switch(c){
     case logError:
@@ -318,7 +320,7 @@ static void emit(log_t *log) {
            categories beyond logInfo to the outside. */
         cb->emit_log(cb, clamp_category(log->c), clamp_category(log->severest_category), buf->msg);
 
-    /* create_log_file_if_needed(log); */
+        create_log_file_if_needed(log);
         if (log->log_file) {
             file_logger(log->log_file, log->log_file, 
                         clamp_category(log->c), clamp_category(log->severest_category), buf->msg);
@@ -425,20 +427,38 @@ static void init_log(log_t *log, jmi_log_options_t* options, jmi_callbacks_t* jm
 
 static void create_log_file_if_needed(log_t *log) {
     if (log->log_file != NULL) return;
+
     if (log->options->copy_log_to_file_flag) {
         /* Create new log file */
         jmi_callbacks_t *cb = log->jmi_callbacks;
         const char *instance_name = cb->instance_name;
         char filename[8000];
 
-        sprintf(filename, "%s_%s.log", cb->model_name,
-                                       instance_name);
-        /* TODO: fopen returns NULL on error
-           ==> will try to reopen the file at the next emit. Do we want this? 
-           Not an issue if create_log_file_if_needed is only called once.*/
+        if(instance_name && instance_name[0]) {
+            sprintf(filename, "%s_%s.xml", cb->model_name,
+                                           instance_name);
+        }
+        else {
+            sprintf(filename, "%s_runtime_log.xml", cb->model_name,
+                                           instance_name);
+        }
+        /* TODO: 
+           create_log_file_if_needed need to be called several times since the options are
+           updated in fmiInitialize() and at that point copy_log_to_file_flag might be set.
+
+           Just to make this safer: if fopen fails - print a message on stderr and
+           clear the option.
+           */
+
         log->log_file = fopen(filename, "w");
 
-        fprintf(log->log_file, "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<JMILog category=\"info\">\n");
+        if(!log->log_file) {
+            fprintf(stderr,"Could not open runtime log file %s", filename);
+            log->options->copy_log_to_file_flag = 0;
+        }
+        else {
+            fprintf(log->log_file, "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<JMILog category=\"info\">\n");
+        }
     }
 }
 

RuntimeLibrary/src/jmi/jmi_me.c
@@ -79,6 +79,9 @@ int jmi_me_init(jmi_callbacks_t* jmi_callbacks, jmi_t* jmi, jmi_string GUID, jmi
         jmi_delete(jmi_);
     	return -1;
     }
+    
+    /* Runtime options may be updated with start values */
+    jmi_update_runtime_options(jmi);
 
     /* Write start values to the pre vector*/
     jmi_copy_pre_values(jmi);

commit 94623b1fab6af1fa908084634fa3450dfa6bfa88

See #3847 Make sure instance name is defined during fmiInstantiate (bug introduced in r6655).
git-svn-id: https://svn.jmodelica.org/trunk@6691 7ae51c65-13d5-4921-9d80-6511ddd1419c

RuntimeLibrary/src/fmi1_me/fmi1_me.c
@@ -150,6 +150,7 @@ fmiComponent fmi1_me_instantiate_model(fmiString instanceName, fmiString GUID, f
     cb->free_memory = functions.freeMemory;
     cb->model_name = jmi_get_model_identifier();       /**< \brief Name of the model (corresponds to a fixed compiled unit name) */
     cb->model_data = component;
+    cb->instance_name = instanceName;  /* set instance name to use under instantiation (to avoid initial malloc)*/
     
     resource_location = jmi_locate_resources(functions.allocateMemory);
     if (!resource_location)
@@ -162,11 +163,12 @@ fmiComponent fmi1_me_instantiate_model(fmiString instanceName, fmiString GUID, f
         return NULL;
     }
 
+    /* fmiString must be copied in and saved */ 
     inst_name_len = strlen(instanceName)+1;
     tmpname = (char*)(fmi1_me_t *)functions.allocateMemory(inst_name_len, sizeof(char));
     strncpy(tmpname, instanceName, inst_name_len);
     component -> fmi_instance_name = tmpname;
-    cb->instance_name = tmpname;  /** < \brief Name of this model instance. */
+    cb->instance_name = tmpname;  /** reset the pointer to use after instantiate. */
 
     return component;
 }