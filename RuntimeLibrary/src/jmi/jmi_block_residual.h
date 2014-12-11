svn2github/JModelica_dot_org

commit 2b76b5a8531fa2fc45206c7d3fecf3422f9019a7

Added code gen for switches and non-real variables that directly impacts the continuous equations. Ticket #3942.
git-svn-id: https://svn.jmodelica.org/trunk@7124 7ae51c65-13d5-4921-9d80-6511ddd1419c

RuntimeLibrary/src/jmi/jmi_block_residual.h
@@ -177,7 +177,9 @@ struct jmi_block_residual_t {
  * @param dF A jmi_block_dir_der_func_t function
  * @param n Integer size of the block of real variables
  * @param n_nr Integer size of the block of non-real variables
+ * @param n_dinr Integer size of the block of directly impacting non-real variables
  * @param n_as Integer size of the number of active switches
+ * @param n_das Integer size of the number of directly active switches
  * @param jacobian_variability Variability of the Jacobian coefficients
  * @param attribute_variability Variability of the variable attributes
  * @param solver Solver to be used for the block
@@ -186,7 +188,7 @@ struct jmi_block_residual_t {
  * @param parent_index Index of parent block.
  * @return Error code.
  */
-int jmi_dae_add_equation_block(jmi_t* jmi, jmi_block_residual_func_t F, jmi_block_dir_der_func_t dF, int n, int n_nr, int n_as, int jacobian_variability, int attribute_variability, jmi_block_solver_kind_t solver, int index, jmi_string_t label, int parent_index);
+int jmi_dae_add_equation_block(jmi_t* jmi, jmi_block_residual_func_t F, jmi_block_dir_der_func_t dF, int n, int n_nr, int n_dinr int n_as, int_das, int jacobian_variability, int attribute_variability, jmi_block_solver_kind_t solver, int index, jmi_string_t label, int parent_index);
 
 /**
  * \brief Register an initialization block residual function in a jmi_t struct.
@@ -196,7 +198,9 @@ int jmi_dae_add_equation_block(jmi_t* jmi, jmi_block_residual_func_t F, jmi_bloc
  * @param dF A jmi_block_dir_der_func_t function
  * @param n Integer size of the block of real variables
  * @param n_nr Integer size of the block of non-real variables
+ * @param n_dinr Integer size of the block of directly impacting non-real variables
  * @param n_as Integer size of the number of active switches
+ * @param n_das Integer size of the number of directly active switches
  * @param jacobian_variability Variability of the Jacobian coefficients
  * @param attribute_variability Variability of the variable attributes
  * @param solver Solver to be used for the block
@@ -205,7 +209,7 @@ int jmi_dae_add_equation_block(jmi_t* jmi, jmi_block_residual_func_t F, jmi_bloc
  * @param parent_index Index of parent block.
  * @return Error code.
  */
-int jmi_dae_init_add_equation_block(jmi_t* jmi, jmi_block_residual_func_t F, jmi_block_dir_der_func_t dF, int n, int n_nr, int n_as, int jacobian_variability, int attribute_variability, jmi_block_solver_kind_t solver, int index, jmi_string_t label, int parent_index);
+int jmi_dae_init_add_equation_block(jmi_t* jmi, jmi_block_residual_func_t F, jmi_block_dir_der_func_t dF, int n, int n_nr, int n_dinr int n_as, int_das, int jacobian_variability, int attribute_variability, jmi_block_solver_kind_t solver, int index, jmi_string_t label, int parent_index);
 
 
 /**

Compiler/ModelicaCBackEnd/src/jastadd/CCodeGen.jrag
@@ -3184,6 +3184,12 @@ aspect CCodeGen {
         for (FVariable fv : localNonRealVariables())
             str.format("%sx[%d] = %d;\n", nextIndent, nonRealEnumerator.next(), fv.valueReference());
         
+        str.print(indent + "} else if (evaluation_mode == JMI_BLOCK_DIRECTLY_IMPACTING_NON_REAL_VALUE_REFERENCE) {\n");
+        // Loop over all non-real variables and write their position in the z-vector
+        Enumerator directImpactNonRealEnumerator = new Enumerator();
+        for (FVariable fv : directlyImpactingNonReals_C())
+            str.format("%sx[%d] = %d;\n", nextIndent, directImpactNonRealEnumerator.next(), fv.valueReference());
+        
         str.print(indent + "} else if (evaluation_mode == JMI_BLOCK_ACTIVE_SWITCH_INDEX) {\n");
         // Loop over all non-real variables and write their position in the z-vector
         Enumerator relExpEnumerator = new Enumerator();
@@ -3191,6 +3197,12 @@ aspect CCodeGen {
             relExp.genBlockResidualFunction_printSwitchIndex_C(p, str, nextIndent, relExpEnumerator);
         for (FRelExp relExp : activeInitialSwitches_C())
             relExp.genBlockResidualFunction_printSwitchIndex_C(p, str, nextIndent, relExpEnumerator);
+        
+        str.print(indent + "} else if (evaluation_mode == JMI_BLOCK_DIRECTLY_ACTIVE_SWITCH_INDEX) {\n");
+        // Loop over all non-real variables and write their position in the z-vector
+        Enumerator directlyActiveSwitchEnumerator = new Enumerator();
+        for (FRelExp relExp : directlyActiveSwitches_C())
+            relExp.genBlockResidualFunction_printSwitchIndex_C(p, str, nextIndent, directlyActiveSwitchEnumerator);
         
         // Equation nominal values
         str.print(indent + "} else if (evaluation_mode == JMI_BLOCK_EQUATION_NOMINAL) {\n");
@@ -3582,7 +3594,9 @@ aspect CCodeGen {
 			str.print("NULL, ");
 		str.print(localUnsolvedVariables().size() + ", "); // Rename to realIterationVariables() and nonRealIterationVariables()?
 		str.print(localNonRealVariables().size() + ", ");
+		str.print(directlyImpactingNonReals_C().size() + ", ");
 		str.print((activeSwitches_C().size() + activeInitialSwitches_C().size()) + ", ");
+		str.print(directlyActiveSwitches_C().size() + ", ");
 		str.print(variability().genJMIVariabilityConstant_C() + ", ");
 		p.C_blockResidualAttributePrinter.printVariability(p, str, indent, this.localUnsolvedVariables());
 		str.print(genJMISolverConstant_C(nleSolver) + ", ");
@@ -3614,6 +3628,8 @@ aspect CCodeGen {
     private Collection<FRelExp> AbstractEquationBlock.inactiveSwitches_C = null;
     private Collection<FRelExp> AbstractEquationBlock.activeInitialSwitches_C = null;
     private Collection<FRelExp> AbstractEquationBlock.inactiveInitialSwitches_C = null;
+    private Collection<FRelExp> AbstractEquationBlock.directlyActiveSwitches_C = null;
+    private Collection<FVariable> AbstractEquationBlock.directlyImpactingNonReals_C = null;
     
     /**
      * Returns all active switches in the block. A switch is considered active
@@ -3650,33 +3666,73 @@ aspect CCodeGen {
     /**
      * Returns all inactive initial switches in the block. A switch is considered
      * active in a block if is located in the block and depends on active
-     * aariables in the block.
+     * variables in the block.
      */
     public Collection<FRelExp> AbstractEquationBlock.inactiveInitialSwitches_C() {
         cacheSwitches_C();
         return inactiveInitialSwitches_C;
     }
     
+    /**
+     * Returns all directly active switches in the block. A switch is
+     * considered active in a block if is located in the block and depends on
+     * active variables in the block. A switch is considered direct if it
+     * diretly affects an continuous equation.
+     */
+    public Collection<FRelExp> AbstractEquationBlock.directlyActiveSwitches_C() {
+        cacheSwitches_C();
+        return directlyActiveSwitches_C;
+    }
+    
+    /**
+     * Returns all directly affecting non reals in the block. A non real is
+     * considered directly affecting in a block if is solved in the block and 
+     * is used in a continuous equation.
+     */
+    public Collection<FVariable> AbstractEquationBlock.directlyImpactingNonReals_C() {
+        cacheSwitches_C();
+        return directlyImpactingNonReals_C;
+    }
+    
     private void AbstractEquationBlock.cacheSwitches_C() {
-        if (activeSwitches_C != null && activeInitialSwitches_C != null && inactiveSwitches_C != null && inactiveInitialSwitches_C != null)
+        if (activeSwitches_C != null && activeInitialSwitches_C != null && inactiveSwitches_C != null &&
+                inactiveInitialSwitches_C != null && directlyActiveSwitches_C != null && directlyImpactingNonReals_C != null)
             return;
-        activeSwitches_C = new ArrayList<FRelExp>();
-        activeInitialSwitches_C = new ArrayList<FRelExp>();
-        inactiveSwitches_C = new ArrayList<FRelExp>();
-        inactiveInitialSwitches_C = new ArrayList<FRelExp>();
-        Collection<FVariable> allVariables = allLocalVariables();
+        activeSwitches_C = new LinkedHashSet<FRelExp>();
+        activeInitialSwitches_C = new LinkedHashSet<FRelExp>();
+        inactiveSwitches_C = new LinkedHashSet<FRelExp>();
+        inactiveInitialSwitches_C = new LinkedHashSet<FRelExp>();
+        directlyActiveSwitches_C = new LinkedHashSet<FRelExp>();
+        directlyImpactingNonReals_C = new LinkedHashSet<FVariable>();
+        
+        Set<FAbstractEquation> realEqns = new HashSet<FAbstractEquation>(localRealEquations());
+        Set<FVariable> nonRealVars = new HashSet<FVariable>(localNonRealVariables());
+        Set<FVariable> allVariables = new HashSet<FVariable>(allLocalVariables());
         for (FAbstractEquation equation : allLocalEquations()) {
+            boolean realEqn = realEqns.contains(equation);
             for (FRelExp relExp : equation.relExpInInitialEquation()) {
-                if (relExp.dependsOn(allVariables))
+                if (relExp.dependsOn(allVariables)) {
                     activeInitialSwitches_C.add(relExp);
-                else
+                    if (realEqn)
+                        directlyActiveSwitches_C.add(relExp);
+                } else {
                     inactiveInitialSwitches_C.add(relExp);
+                }
             }
             for (FRelExp relExp : equation.relExpInEquation()) {
-                if (relExp.dependsOn(allVariables))
+                if (relExp.dependsOn(allVariables)) {
                     activeSwitches_C.add(relExp);
-                else
+                    if (realEqn)
+                        directlyActiveSwitches_C.add(relExp);
+                } else {
                     inactiveSwitches_C.add(relExp);
+                }
+            }
+            if (realEqn) {
+                for (FVariable var : equation.referencedFVariables()) {
+                    if (nonRealVars.contains(var))
+                        directlyImpactingNonReals_C.add(var);
+                }
             }
         }
     }

Compiler/ModelicaCBackEnd/src/test/LocalIterationBack.mo
@@ -47,7 +47,9 @@ static int dae_block_1(jmi_t* jmi, jmi_real_t* x, jmi_real_t* residual, int eval
     } else if (evaluation_mode == JMI_BLOCK_VALUE_REFERENCE) {
         x[0] = 1;
     } else if (evaluation_mode == JMI_BLOCK_NON_REAL_VALUE_REFERENCE) {
+    } else if (evaluation_mode == JMI_BLOCK_DIRECTLY_IMPACTING_NON_REAL_VALUE_REFERENCE) {
     } else if (evaluation_mode == JMI_BLOCK_ACTIVE_SWITCH_INDEX) {
+    } else if (evaluation_mode == JMI_BLOCK_DIRECTLY_ACTIVE_SWITCH_INDEX) {
     } else if (evaluation_mode == JMI_BLOCK_EQUATION_NOMINAL) {
     } else if (evaluation_mode == JMI_BLOCK_INITIALIZE) {
         x[0] = _b_1;
@@ -73,7 +75,9 @@ static int dae_block_0(jmi_t* jmi, jmi_real_t* x, jmi_real_t* residual, int eval
     } else if (evaluation_mode == JMI_BLOCK_VALUE_REFERENCE) {
         x[0] = 2;
     } else if (evaluation_mode == JMI_BLOCK_NON_REAL_VALUE_REFERENCE) {
+    } else if (evaluation_mode == JMI_BLOCK_DIRECTLY_IMPACTING_NON_REAL_VALUE_REFERENCE) {
     } else if (evaluation_mode == JMI_BLOCK_ACTIVE_SWITCH_INDEX) {
+    } else if (evaluation_mode == JMI_BLOCK_DIRECTLY_ACTIVE_SWITCH_INDEX) {
     } else if (evaluation_mode == JMI_BLOCK_EQUATION_NOMINAL) {
     } else if (evaluation_mode == JMI_BLOCK_INITIALIZE) {
         x[0] = _c_2;
@@ -91,8 +95,8 @@ static int dae_block_0(jmi_t* jmi, jmi_real_t* x, jmi_real_t* residual, int eval
 }
 
 
-    jmi_dae_add_equation_block(*jmi, dae_block_1, NULL, 1, 0, 0, JMI_CONTINUOUS_VARIABILITY, JMI_CONSTANT_VARIABILITY, JMI_KINSOL_SOLVER, 1, \"1.1\", 0);
-    jmi_dae_add_equation_block(*jmi, dae_block_0, NULL, 1, 0, 0, JMI_CONTINUOUS_VARIABILITY, JMI_CONSTANT_VARIABILITY, JMI_KINSOL_SOLVER, 0, \"1\", -1);
+    jmi_dae_add_equation_block(*jmi, dae_block_1, NULL, 1, 0, 0, 0, 0, JMI_CONTINUOUS_VARIABILITY, JMI_CONSTANT_VARIABILITY, JMI_KINSOL_SOLVER, 1, \"1.1\", 0);
+    jmi_dae_add_equation_block(*jmi, dae_block_0, NULL, 1, 0, 0, 0, 0, JMI_CONTINUOUS_VARIABILITY, JMI_CONSTANT_VARIABILITY, JMI_KINSOL_SOLVER, 0, \"1\", -1);
 ")})));
     end CGenTest1;
 
@@ -190,8 +194,8 @@ static int dae_block_dir_der_0(jmi_t* jmi, jmi_real_t* x, jmi_real_t* dx,jmi_rea
 }
 
 
-    jmi_dae_add_equation_block(*jmi, dae_block_1, dae_block_dir_der_1, 1, 0, 0, JMI_CONTINUOUS_VARIABILITY, JMI_CONSTANT_VARIABILITY, JMI_KINSOL_SOLVER, 1, \"1.1\", 0);
-    jmi_dae_add_equation_block(*jmi, dae_block_0, dae_block_dir_der_0, 1, 0, 0, JMI_CONTINUOUS_VARIABILITY, JMI_CONSTANT_VARIABILITY, JMI_KINSOL_SOLVER, 0, \"1\", -1);
+    jmi_dae_add_equation_block(*jmi, dae_block_1, dae_block_dir_der_1, 1, 0, 0, 0, 0, JMI_CONTINUOUS_VARIABILITY, JMI_CONSTANT_VARIABILITY, JMI_KINSOL_SOLVER, 1, \"1.1\", 0);
+    jmi_dae_add_equation_block(*jmi, dae_block_0, dae_block_dir_der_0, 1, 0, 0, 0, 0, JMI_CONTINUOUS_VARIABILITY, JMI_CONSTANT_VARIABILITY, JMI_KINSOL_SOLVER, 0, \"1\", -1);
 ")})));
     end CADGenTest1;
 

RuntimeLibrary/src/jmi/jmi_block_residual.c
@@ -35,7 +35,7 @@
 #define nbr_allocated_iterations 30
 
 
-int jmi_dae_add_equation_block(jmi_t* jmi, jmi_block_residual_func_t F, jmi_block_dir_der_func_t dF, int n, int n_nr, int n_sw, int jacobian_variability, int attribute_variability, jmi_block_solver_kind_t solver, int index, jmi_string_t label, int parent_index) {
+int jmi_dae_add_equation_block(jmi_t* jmi, jmi_block_residual_func_t F, jmi_block_dir_der_func_t dF, int n, int n_nr, int n_dinr, int n_sw, int n_das, int jacobian_variability, int attribute_variability, jmi_block_solver_kind_t solver, int index, jmi_string_t label, int parent_index) {
     jmi_block_residual_t* b;
     int flag;
     flag = jmi_new_block_residual(&b,jmi, solver, F, dF, n, n_nr, n_sw, jacobian_variability, index, label);
@@ -49,7 +49,7 @@ int jmi_dae_add_equation_block(jmi_t* jmi, jmi_block_residual_func_t F, jmi_bloc
     return flag;
 }
 
-int jmi_dae_init_add_equation_block(jmi_t* jmi, jmi_block_residual_func_t F, jmi_block_dir_der_func_t dF, int n, int n_nr, int n_sw, int jacobian_variability, int attribute_variability, jmi_block_solver_kind_t solver, int index, jmi_string_t label, int parent_index) {
+int jmi_dae_init_add_equation_block(jmi_t* jmi, jmi_block_residual_func_t F, jmi_block_dir_der_func_t dF, int n, int n_nr, int n_dinr, int n_sw, int n_das, int jacobian_variability, int attribute_variability, jmi_block_solver_kind_t solver, int index, jmi_string_t label, int parent_index) {
     jmi_block_residual_t* b;
     int flag;
     flag = jmi_new_block_residual(&b,jmi, solver, F, dF, n, n_nr, n_sw, jacobian_variability, index, label);

RuntimeLibrary/src/jmi/jmi_block_solver.h
@@ -28,20 +28,22 @@
 
 /** \brief Evaluation modes for the residual function.*/
 /** TODO: convert into enum */
-#define JMI_BLOCK_INITIALIZE               0
-#define JMI_BLOCK_EVALUATE                 1
-#define JMI_BLOCK_WRITE_BACK               2
-#define JMI_BLOCK_EVALUATE_INACTIVE        4
-#define JMI_BLOCK_EVALUATE_NON_REALS       8
-#define JMI_BLOCK_MIN                      16
-#define JMI_BLOCK_MAX                      32
-#define JMI_BLOCK_NOMINAL                  64
-#define JMI_BLOCK_EVALUATE_JACOBIAN        128
-#define JMI_BLOCK_EQUATION_NOMINAL         256
-#define JMI_BLOCK_VALUE_REFERENCE          512
-#define JMI_BLOCK_NON_REAL_VALUE_REFERENCE 1024
-#define JMI_BLOCK_ACTIVE_SWITCH_INDEX      2048
-#define JMI_BLOCK_START                    4096
+#define JMI_BLOCK_INITIALIZE                                    0
+#define JMI_BLOCK_EVALUATE                                      1
+#define JMI_BLOCK_WRITE_BACK                                    2
+#define JMI_BLOCK_EVALUATE_INACTIVE                             4
+#define JMI_BLOCK_EVALUATE_NON_REALS                            8
+#define JMI_BLOCK_MIN                                           16
+#define JMI_BLOCK_MAX                                           32
+#define JMI_BLOCK_NOMINAL                                       64
+#define JMI_BLOCK_EVALUATE_JACOBIAN                             128
+#define JMI_BLOCK_EQUATION_NOMINAL                              256
+#define JMI_BLOCK_VALUE_REFERENCE                               512
+#define JMI_BLOCK_NON_REAL_VALUE_REFERENCE                      1024
+#define JMI_BLOCK_ACTIVE_SWITCH_INDEX                           2048
+#define JMI_BLOCK_START                                         4096
+#define JMI_BLOCK_DIRECTLY_IMPACTING_NON_REAL_VALUE_REFERENCE   8192
+#define JMI_BLOCK_DIRECTLY_ACTIVE_SWITCH_INDEX                  16384
 
 /** \brief Jacobian variability for the linear solver */
 typedef enum jmi_block_solver_jac_variability_t {

commit 2129916f981d6d5210eebe767a3d50edd951c081

Fixed syntax error that was introduced in r7124. Ticket #3942.
git-svn-id: https://svn.jmodelica.org/trunk@7125 7ae51c65-13d5-4921-9d80-6511ddd1419c

RuntimeLibrary/src/jmi/jmi_block_residual.h
@@ -188,7 +188,7 @@ struct jmi_block_residual_t {
  * @param parent_index Index of parent block.
  * @return Error code.
  */
-int jmi_dae_add_equation_block(jmi_t* jmi, jmi_block_residual_func_t F, jmi_block_dir_der_func_t dF, int n, int n_nr, int n_dinr int n_as, int_das, int jacobian_variability, int attribute_variability, jmi_block_solver_kind_t solver, int index, jmi_string_t label, int parent_index);
+int jmi_dae_add_equation_block(jmi_t* jmi, jmi_block_residual_func_t F, jmi_block_dir_der_func_t dF, int n, int n_nr, int n_dinr, int n_as, int_das, int jacobian_variability, int attribute_variability, jmi_block_solver_kind_t solver, int index, jmi_string_t label, int parent_index);
 
 /**
  * \brief Register an initialization block residual function in a jmi_t struct.
@@ -209,7 +209,7 @@ int jmi_dae_add_equation_block(jmi_t* jmi, jmi_block_residual_func_t F, jmi_bloc
  * @param parent_index Index of parent block.
  * @return Error code.
  */
-int jmi_dae_init_add_equation_block(jmi_t* jmi, jmi_block_residual_func_t F, jmi_block_dir_der_func_t dF, int n, int n_nr, int n_dinr int n_as, int_das, int jacobian_variability, int attribute_variability, jmi_block_solver_kind_t solver, int index, jmi_string_t label, int parent_index);
+int jmi_dae_init_add_equation_block(jmi_t* jmi, jmi_block_residual_func_t F, jmi_block_dir_der_func_t dF, int n, int n_nr, int n_dinr, int n_as, int_das, int jacobian_variability, int attribute_variability, jmi_block_solver_kind_t solver, int index, jmi_string_t label, int parent_index);
 
 
 /**