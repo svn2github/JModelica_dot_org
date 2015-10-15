svn2github/JModelica_dot_org

commit 866ee4456f5949877beca05cfef63c1789fd3239

Fixed expansion of inStream() when some flows can be inferred to be constant. (#4617)
git-svn-id: https://svn.jmodelica.org/trunk@8105 7ae51c65-13d5-4921-9d80-6511ddd1419c

Compiler/ModelicaFrontEnd/src/test/StreamTests.mo
@@ -709,7 +709,7 @@ model StreamN2M1
         StreamConnector c;
     end A;
     
-    A a1(c(s=1, p=4));
+    A a1(c(s=1, p=4, f=time));
     A a2(c(s=2));
     StreamConnector c;
     Real x1 = inStream(a1.c.s);
@@ -720,31 +720,38 @@ equation
     connect(a1.c, c);
 
     annotation(__JModelica(UnitTesting(tests={
-        FlatteningTestCase(
+        TransformCanonicalTestCase(
             name="StreamN2M1",
             description="Test stream connectors connected N=2, M=1",
             eliminate_alias_variables=false,
             variability_propagation=false,
             flatModel="
 fclass StreamTests.StreamN2M1
- Real a1.c.p = 4;
+ Real a1.c.p;
  Real a1.c.f;
- Real a1.c.s = 1;
+ Real a1.c.s;
  Real a2.c.p;
  Real a2.c.f;
- Real a2.c.s = 2;
+ Real a2.c.s;
  Real c.p;
  Real c.f;
  Real c.s;
- Real x1 = inStream(a1.c.s);
- Real x2 = inStream(a2.c.s);
- Real x3 = inStream(c.s);
+ Real x1;
+ Real x2;
+ Real x3;
 equation
  a1.c.f + a2.c.f - c.f = 0;
  a1.c.p = a2.c.p;
  a2.c.p = c.p;
  c.s = (max(- a1.c.f, 1.0E-8) * a1.c.s + max(- a2.c.f, 1.0E-8) * a2.c.s) / (max(- a1.c.f, 1.0E-8) + max(- a2.c.f, 1.0E-8));
  c.f = 0;
+ a1.c.p = 4;
+ a1.c.f = time;
+ a1.c.s = 1;
+ a2.c.s = 2;
+ x1 = (max(- a2.c.f, 1.0E-8) * a2.c.s + max(c.f, 1.0E-8) * c.s) / (max(- a2.c.f, 1.0E-8) + max(c.f, 1.0E-8));
+ x2 = (max(- a1.c.f, 1.0E-8) * a1.c.s + max(c.f, 1.0E-8) * c.s) / (max(- a1.c.f, 1.0E-8) + max(c.f, 1.0E-8));
+ x3 = c.s;
 end StreamTests.StreamN2M1;
 ")})));
 end StreamN2M1;
@@ -766,33 +773,38 @@ equation
     connect(a.c, c2);
 
     annotation(__JModelica(UnitTesting(tests={
-        FlatteningTestCase(
+        TransformCanonicalTestCase(
             name="StreamN1M2",
             description="Test stream connectors connected N=1, M=2",
             eliminate_alias_variables=false,
             variability_propagation=false,
             flatModel="
 fclass StreamTests.StreamN1M2
- Real a.c.p = 2;
+ Real a.c.p;
  Real a.c.f;
- Real a.c.s = 1;
+ Real a.c.s;
  Real c1.p;
  Real c1.f;
  Real c1.s;
  Real c2.p;
  Real c2.f;
  Real c2.s;
- Real x1 = inStream(a.c.s);
- Real x2 = inStream(c1.s);
- Real x3 = inStream(c2.s);
+ Real x1;
+ Real x2;
+ Real x3;
 equation
  a.c.f - c1.f - c2.f = 0;
  a.c.p = c1.p;
  c1.p = c2.p;
- c1.s = (max(- a.c.f, 1.0E-8) * a.c.s + max(c2.f, 1.0E-8) * inStream(c2.s)) / (max(- a.c.f, 1.0E-8) + max(c2.f, 1.0E-8));
- c2.s = (max(- a.c.f, 1.0E-8) * a.c.s + max(c1.f, 1.0E-8) * inStream(c1.s)) / (max(- a.c.f, 1.0E-8) + max(c1.f, 1.0E-8));
+ c1.s = (max(- a.c.f, 1.0E-8) * a.c.s + max(c2.f, 1.0E-8) * c2.s) / (max(- a.c.f, 1.0E-8) + max(c2.f, 1.0E-8));
+ c2.s = (max(- a.c.f, 1.0E-8) * a.c.s + max(c1.f, 1.0E-8) * c1.s) / (max(- a.c.f, 1.0E-8) + max(c1.f, 1.0E-8));
  c1.f = 0;
  c2.f = 0;
+ a.c.p = 2;
+ a.c.s = 1;
+ x1 = (max(c1.f, 1.0E-8) * c1.s + max(c2.f, 1.0E-8) * c2.s) / (max(c1.f, 1.0E-8) + max(c2.f, 1.0E-8));
+ x2 = c1.s;
+ x3 = c2.s;
 end StreamTests.StreamN1M2;
 ")})));
 end StreamN1M2;
@@ -1458,6 +1470,153 @@ equation
 end StreamTests.InStreamDer1;
 ")})));
 end InStreamDer1;
+
+
+model StreamWithConst1
+    model A
+        StreamConnector c;
+    end A;
+	    
+    A a1(c(f = 0, p = 0, s = 2 * time)), a2(c(f = time, s = 3 * time)), a3(c(s = 4 * time));
+    Real x1, x2, x3;
+equation
+    connect(a1.c, a2.c);
+    connect(a2.c, a3.c);
+    x1 = inStream(a1.c.s);
+    x2 = inStream(a2.c.s);
+    x3 = inStream(a3.c.s);
+
+    annotation(__JModelica(UnitTesting(tests={
+        TransformCanonicalTestCase(
+            name="StreamWithConst1",
+            description="Test stream connectors connected N=0, M=3, with some constant flows",
+            flatModel="
+fclass StreamTests.StreamWithConst1
+ constant Real a1.c.p = 0;
+ constant Real a1.c.f = 0;
+ Real a1.c.s;
+ Real a2.c.f;
+ Real x1;
+ Real x2;
+ Real x3;
+ constant Real a2.c.p = 0;
+ constant Real a3.c.p = 0;
+equation
+ x1 = (max(- a2.c.f, 1.0E-8) * x3 + max(a2.c.f, 1.0E-8) * x2) / (max(- a2.c.f, 1.0E-8) + max(a2.c.f, 1.0E-8));
+ a1.c.s = 2 * time;
+ a2.c.f = time;
+ x3 = 3 * time;
+ x2 = 4 * time;
+end StreamTests.StreamWithConst1;
+")})));
+end StreamWithConst1;
+
+
+model StreamWithConst2
+    model A
+        StreamConnector c;
+    end A;
+    
+    A a1(c(s=1, p=4, f=1));
+    A a2(c(s=2));
+    StreamConnector c1;
+    StreamConnector c2;
+    Real x1 = inStream(a1.c.s);
+    Real x2 = inStream(a2.c.s);
+equation
+    connect(a1.c, a2.c);
+    connect(a1.c, c1);
+    connect(a1.c, c2);
+
+    annotation(__JModelica(UnitTesting(tests={
+        TransformCanonicalTestCase(
+            name="StreamWithConst2",
+            description="Test stream connectors connected N=2, M=2, with some constant flows",
+            flatModel="
+fclass StreamTests.StreamWithConst2
+ constant Real a1.c.f = 1;
+ constant Real a1.c.s = 1;
+ constant Real a2.c.f = -1.0;
+ constant Real a2.c.s = 2;
+ constant Real c1.p = 4;
+ constant Real c1.f = 0;
+ Real c1.s;
+ constant Real c2.f = 0;
+ Real c2.s;
+ Real x1;
+ Real x2;
+ constant Real a1.c.p = 4;
+ constant Real a2.c.p = 4;
+ constant Real c2.p = 4;
+equation
+ c1.s = 2.0;
+ c2.s = 2.0;
+ x1 = 2.0;
+ x2 = 2.0;
+end StreamTests.StreamWithConst2;
+")})));
+end StreamWithConst2;
+
+
+model StreamWithConst3
+    model A
+        StreamConnector c;
+    end A;
+    
+    A a1[2](c(s={1,2*time}, p={3,4*time}, f={1,time}));
+    A a2[2](c(s={5,6*time}));
+    StreamConnector c1[2];
+    StreamConnector c2[2];
+    Real x1[2] = inStream(a1.c.s);
+    Real x2[2] = inStream(a2.c.s);
+equation
+    connect(a1.c, a2.c);
+    connect(a1.c, c1);
+    connect(a1.c, c2);
+
+    annotation(__JModelica(UnitTesting(tests={
+        TransformCanonicalTestCase(
+            name="StreamWithConst3",
+            description="",
+            flatModel="
+fclass StreamTests.StreamWithConst3
+ constant Real a1[1].c.f = 1;
+ constant Real a1[1].c.s = 1;
+ Real a1[2].c.f;
+ constant Real a2[1].c.f = -1.0;
+ constant Real a2[1].c.s = 5;
+ constant Real c1[1].p = 3;
+ constant Real c1[1].f = 0;
+ Real c1[1].s;
+ Real c1[2].p;
+ constant Real c1[2].f = 0;
+ Real c1[2].s;
+ constant Real c2[1].f = 0;
+ Real c2[1].s;
+ constant Real c2[2].f = 0;
+ Real c2[2].s;
+ Real x1[1];
+ Real x1[2];
+ Real x2[1];
+ Real x2[2];
+ constant Real a1[1].c.p = 3;
+ constant Real a2[1].c.p = 3;
+ constant Real c2[1].p = 3;
+equation
+ c1[1].s = 5.0;
+ c2[1].s = 5.0;
+ c1[2].s = (max(- a1[2].c.f, 1.0E-8) * x2[2] + max(a1[2].c.f, 1.0E-8) * x1[2]) / (max(- a1[2].c.f, 1.0E-8) + max(a1[2].c.f, 1.0E-8));
+ c2[2].s = (max(- a1[2].c.f, 1.0E-8) * x2[2] + max(a1[2].c.f, 1.0E-8) * x1[2]) / (max(- a1[2].c.f, 1.0E-8) + max(a1[2].c.f, 1.0E-8));
+ c1[2].p = 4 * time;
+ a1[2].c.f = time;
+ x2[2] = 2 * time;
+ x1[2] = 6 * time;
+ x1[1] = 5.0;
+ x2[1] = 5.0;
+end StreamTests.StreamWithConst3;
+")})));
+end StreamWithConst3;
+
 
 // TODO: Add error tests (e.g. stream connector without flow)
 

Compiler/ModelicaFlatTree/src/jastadd/ConstantEvaluation.jrag
@@ -2607,6 +2607,35 @@ aspect ConstantEvaluation {
     eq FLogExp.cevalCalc(VariableEvaluator evaluator)   = new CValueReal(StrictMath.log(getFExp().ceval(evaluator).realValue()));
     eq FLog10Exp.cevalCalc(VariableEvaluator evaluator) = new CValueReal(StrictMath.log10(getFExp().ceval(evaluator).realValue()));
 
+    eq FExInStream.cevalCalc(VariableEvaluator evaluator) {
+        double eps = -1.0;
+        double val = 0.0;
+        double sum = 0.0;
+        double div = 0.0;
+        int numContributors = 0;
+        for (int i = 0, n = numVars(); i < n; i++) {
+            boolean outside = isOutsideExp(i).ceval(evaluator).booleanValue();
+            double flow = flowExp(i).ceval(evaluator).realValue();
+            if (!outside)
+                flow = -flow;
+            if (flow > 0.0) {
+                numContributors++;
+                if (eps < 0.0)
+                    eps = getEps().ceval(evaluator).realValue();
+                val = streamExp(i).ceval(evaluator).realValue();
+                flow = Math.max(flow, eps);
+                sum += flow * val;
+                div += flow;
+            }
+        }
+        if (numContributors == 0) {
+            val = getDefault().ceval(evaluator).realValue();
+        } else if (numContributors > 1) {
+            val = sum / div;
+        }
+        return new CValueReal(val);
+    }
+
     syn CValue FSubscript.ceval() = ceval(defaultVariableEvaluator());
 	syn CValue FSubscript.ceval(VariableEvaluator evaluator) = CValue.UNSUPPORTED;
 	eq FExpSubscript.ceval(VariableEvaluator evaluator)      = getFExp().ceval(evaluator);

Compiler/ModelicaFlatTree/src/jastadd/Constructors.jrag
@@ -1091,45 +1091,46 @@ aspect InheritedFactoryMethods {
 	public FExpExp   FExpExp.createEmptyNode()   { return new FExpExp();   }
 	public FLogExp   FLogExp.createEmptyNode()   { return new FLogExp();   }
 	public FLog10Exp FLog10Exp.createEmptyNode() { return new FLog10Exp(); }
-	
-	// Built-in function calls and function call-like operators
-	public FNdimsExp       FNdimsExp.createEmptyNode()       { return new FNdimsExp();       }
-	public FSizeExp        FSizeExp.createEmptyNode()        { return new FSizeExp();        }
-	public FAbsExp         FAbsExp.createEmptyNode()         { return new FAbsExp();         }
-	public FSignExp        FSignExp.createEmptyNode()        { return new FSignExp();        }
-	public FSqrtExp        FSqrtExp.createEmptyNode()        { return new FSqrtExp();        }
-	public FEnumIntegerExp FEnumIntegerExp.createEmptyNode() { return new FEnumIntegerExp(); }
-	public FStringExp      FStringExp.createEmptyNode()      { return new FStringExp();      }
-	public FDivFuncExp     FDivFuncExp.createEmptyNode()     { return new FDivFuncExp();     }
-	public FModFuncExp     FModFuncExp.createEmptyNode()     { return new FModFuncExp();     }
-	public FRemFuncExp     FRemFuncExp.createEmptyNode()     { return new FRemFuncExp();     }
-	public FCeilFuncExp    FCeilFuncExp.createEmptyNode()    { return new FCeilFuncExp();    }
-	public FFloorFuncExp   FFloorFuncExp.createEmptyNode()   { return new FFloorFuncExp();   }
-	public FIntegerFuncExp FIntegerFuncExp.createEmptyNode() { return new FIntegerFuncExp(); }
-	public FScalarExp      FScalarExp.createEmptyNode()      { return new FScalarExp();      }
-	public FVectorExp      FVectorExp.createEmptyNode()      { return new FVectorExp();      }
-	public FMatrixExp      FMatrixExp.createEmptyNode()      { return new FMatrixExp();      }
-	public FTranspose      FTranspose.createEmptyNode()      { return new FTranspose();      }
-	public FSymmetric      FSymmetric.createEmptyNode()      { return new FSymmetric();      }
-	public FCross          FCross.createEmptyNode()          { return new FCross();          }
-	public FSkew           FSkew.createEmptyNode()           { return new FSkew();           }
-	public FOuterProduct   FOuterProduct.createEmptyNode()   { return new FOuterProduct();   }
-	public FIdentity       FIdentity.createEmptyNode()       { return new FIdentity();       }
-	public FDiagonal       FDiagonal.createEmptyNode()       { return new FDiagonal();       }
-	public FOnes           FOnes.createEmptyNode()           { return new FOnes();           }
-	public FZeros          FZeros.createEmptyNode()          { return new FZeros();          }
-	public FFillExp        FFillExp.createEmptyNode()        { return new FFillExp();        }
-	public FMinExp         FMinExp.createEmptyNode()         { return new FMinExp();         }
-	public FMaxExp         FMaxExp.createEmptyNode()         { return new FMaxExp();         }
-	public FSumExp         FSumExp.createEmptyNode()         { return new FSumExp();         }
-	public FProductExp     FProductExp.createEmptyNode()     { return new FProductExp();     }
-	public FHomotopyExp    FHomotopyExp.createEmptyNode()    { return new FHomotopyExp();    }
-	public FSemiLinearExp  FSemiLinearExp.createEmptyNode()  { return new FSemiLinearExp();  }
-	public FInStream       FInStream.createEmptyNode()       { return new FInStream();       }
-	public FDerInStream    FDerInStream.createEmptyNode()    { return new FDerInStream();    }
-	public FActualStream   FActualStream.createEmptyNode()   { return new FActualStream();   }
-	public FGetInstanceName FGetInstanceName.createEmptyNode() { return new FGetInstanceName(); }
-	
+
+    // Built-in function calls and function call-like operators
+    public FNdimsExp        FNdimsExp.createEmptyNode()        { return new FNdimsExp();        }
+    public FSizeExp         FSizeExp.createEmptyNode()         { return new FSizeExp();         }
+    public FAbsExp          FAbsExp.createEmptyNode()          { return new FAbsExp();          }
+    public FSignExp         FSignExp.createEmptyNode()         { return new FSignExp();         }
+    public FSqrtExp         FSqrtExp.createEmptyNode()         { return new FSqrtExp();         }
+    public FEnumIntegerExp  FEnumIntegerExp.createEmptyNode()  { return new FEnumIntegerExp();  }
+    public FStringExp       FStringExp.createEmptyNode()       { return new FStringExp();       }
+    public FDivFuncExp      FDivFuncExp.createEmptyNode()      { return new FDivFuncExp();      }
+    public FModFuncExp      FModFuncExp.createEmptyNode()      { return new FModFuncExp();      }
+    public FRemFuncExp      FRemFuncExp.createEmptyNode()      { return new FRemFuncExp();      }
+    public FCeilFuncExp     FCeilFuncExp.createEmptyNode()     { return new FCeilFuncExp();     }
+    public FFloorFuncExp    FFloorFuncExp.createEmptyNode()    { return new FFloorFuncExp();    }
+    public FIntegerFuncExp  FIntegerFuncExp.createEmptyNode()  { return new FIntegerFuncExp();  }
+    public FScalarExp       FScalarExp.createEmptyNode()       { return new FScalarExp();       }
+    public FVectorExp       FVectorExp.createEmptyNode()       { return new FVectorExp();       }
+    public FMatrixExp       FMatrixExp.createEmptyNode()       { return new FMatrixExp();       }
+    public FTranspose       FTranspose.createEmptyNode()       { return new FTranspose();       }
+    public FSymmetric       FSymmetric.createEmptyNode()       { return new FSymmetric();       }
+    public FCross           FCross.createEmptyNode()           { return new FCross();           }
+    public FSkew            FSkew.createEmptyNode()            { return new FSkew();            }
+    public FOuterProduct    FOuterProduct.createEmptyNode()    { return new FOuterProduct();    }
+    public FIdentity        FIdentity.createEmptyNode()        { return new FIdentity();        }
+    public FDiagonal        FDiagonal.createEmptyNode()        { return new FDiagonal();        }
+    public FOnes            FOnes.createEmptyNode()            { return new FOnes();            }
+    public FZeros           FZeros.createEmptyNode()           { return new FZeros();           }
+    public FFillExp         FFillExp.createEmptyNode()         { return new FFillExp();         }
+    public FMinExp          FMinExp.createEmptyNode()          { return new FMinExp();          }
+    public FMaxExp          FMaxExp.createEmptyNode()          { return new FMaxExp();          }
+    public FSumExp          FSumExp.createEmptyNode()          { return new FSumExp();          }
+    public FProductExp      FProductExp.createEmptyNode()      { return new FProductExp();      }
+    public FHomotopyExp     FHomotopyExp.createEmptyNode()     { return new FHomotopyExp();     }
+    public FSemiLinearExp   FSemiLinearExp.createEmptyNode()   { return new FSemiLinearExp();   }
+    public FInStream        FInStream.createEmptyNode()        { return new FInStream();        }
+    public FDerInStream     FDerInStream.createEmptyNode()     { return new FDerInStream();     }
+    public FActualStream    FActualStream.createEmptyNode()    { return new FActualStream();    }
+    public FExInStream      FExInStream.createEmptyNode()      { return new FExInStream();      }
+    public FGetInstanceName FGetInstanceName.createEmptyNode() { return new FGetInstanceName(); }
+
 	// Operators for overconstrained connection graphs
 	public FConnBranch        FConnBranch.createEmptyNode()        { return new FConnBranch();        }
 	public FConnRoot          FConnRoot.createEmptyNode()          { return new FConnRoot();          }

Compiler/ModelicaFlatTree/src/jastadd/FlatAPI.jrag
@@ -2559,6 +2559,63 @@ aspect Attributes {
     
     
     /**
+     * Return the maximum numeric value that this expression can take, considering min/max values of variables.
+     * 
+     * Only implemented for a small subset of expressions:
+     *  - flat access to Real or Integer
+     *  - numeric literal
+     *  - negation
+     */
+    syn double FExp.maxRealValue() {
+        throw new UnsupportedOperationException();
+    }
+    
+    /**
+     * Return the minimum numeric value that this expression can take, considering min/max values of variables.
+     * 
+     * Only implemented for a small subset of expressions:
+     *  - flat access to Real or Integer
+     *  - numeric literal
+     *  - negation
+     */
+    syn double FExp.minRealValue() {
+        throw new UnsupportedOperationException();
+    }
+    
+    eq FRealLitExp.maxRealValue()    = getValue();
+    eq FRealLitExp.minRealValue()    = getValue();
+    eq FIntegerLitExp.maxRealValue() = getValue();
+    eq FIntegerLitExp.minRealValue() = getValue();
+    eq FNegExp.maxRealValue()        = -getFExp().minRealValue();
+    eq FNegExp.minRealValue()        = -getFExp().maxRealValue();
+    eq FIdUseExp.maxRealValue()      = getFIdUse().myFV().maxRealValue();
+    eq FIdUseExp.minRealValue()      = getFIdUse().myFV().minRealValue();
+
+    /**
+     * Return the maximum numeric value that this variable can take.
+     * 
+     * Only implemented for Real and Integer variables.
+     */
+    syn double FAbstractVariable.maxRealValue() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Return the minimum numeric value that this variable can take.
+     * 
+     * Only implemented for Real and Integer variables.
+     */
+    syn double FAbstractVariable.minRealValue() {
+        throw new UnsupportedOperationException();
+    }
+    
+    eq FRealVariable.maxRealValue()    = variability().knownParameterOrLess() ? ceval().realValue() : maxAttribute();
+    eq FRealVariable.minRealValue()    = variability().knownParameterOrLess() ? ceval().realValue() : minAttribute();
+    eq FIntegerVariable.maxRealValue() = variability().knownParameterOrLess() ? ceval().intValue() : maxAttribute();
+    eq FIntegerVariable.minRealValue() = variability().knownParameterOrLess() ? ceval().intValue() : minAttribute();
+    
+    
+    /**
      * Check if this attribute is in an FVariable.
      */
     inh boolean FAttribute.isInFV();

Compiler/ModelicaFlatTree/src/jastadd/PrettyPrint.jrag
@@ -1906,6 +1906,7 @@ aspect FlatPrettyPrint {
 	eq FSemiLinearExp.builtInName()      = "semiLinear";
 	eq FInStream.builtInName()           = "inStream";
 	eq FDerInStream.builtInName()        = "derInStream";
+    eq FExInStream.builtInName()         = "inStreamExpansion";
 	eq FActualStream.builtInName()       = "actualStream";
 	eq FGetInstanceName.builtInName()    = "getInstanceName";
 	eq FConnBranch.builtInName()         = "Connections.branch";

Compiler/ModelicaFlatTree/src/jastadd/TypeAnalysis.jrag
@@ -429,6 +429,8 @@ aspect FlatTypeAnalysis {
 
     eq FVectUnaryBuiltIn.type() = scalarType().sizedType(size());
 
+    eq FExInStream.type() = fRealType(size());
+
     eq FSmoothExp.type() = getFExp().type();
 
     eq FConnectionsOp.type() = fNoType();

Compiler/ModelicaFlatTree/src/jastadd/Variability.jrag
@@ -565,6 +565,22 @@ aspect Variability {
     eq FZeros.expVariability()     = fConstant();
     eq FFillExp.expVariability()   = getFillExp().variability();
 
+    eq FExInStream.expVariability() {
+        FTypePrefixVariability stream = getDefault().variability();
+        FTypePrefixVariability total = fConstant();
+        int n = 0;
+        for (InStreamPart cont : contributors()) {
+            stream = cont.stream.variability();
+            total = total.combine(stream).combine(cont.flow.variability());
+            n++;
+        }
+        if (n < 2) {
+            return stream;
+        } else {
+            return total;
+        }
+    }
+
     eq FEdgeExp.expVariability()   = getFExp().variability().combineDown(fDiscrete());
     eq FChangeExp.expVariability() = getFExp().variability().combineDown(fDiscrete());
     eq FLoadResource.expVariability() = fParameter();

Compiler/ModelicaFlatTree/src/jastadd/ast/FlatModelica.ast
@@ -1038,7 +1038,7 @@ FOverflowIntLitExp : FRealLitExp ::= <String>;
 /**
  * Boolean literal expression.
  */
-abstract FBooleanLitExp : FLitExp ::=;
+abstract FBooleanLitExp : FLitExp;
 
 /**
  * True boolean literal expression.
@@ -1705,7 +1705,7 @@ abstract FStreamBuiltIn : FVectUnaryBuiltIn;
 FInStream : FStreamBuiltIn;
 
 /**
- * Derivative of instream. Temporary expression created by der(inStream()) rewrites
+ * Derivative of inStream(). Temporary expression created by der(inStream()) rewrites
  * and removed after connection sets are built.
  */
 FDerInStream : FInStream;
@@ -1714,6 +1714,16 @@ FDerInStream : FInStream;
  * ActualStream built-in function.
  */
 FActualStream : FStreamBuiltIn;
+
+/**
+ * Expanded version of FInStream, used temporarily during symbolic manipulations.
+ *
+ * Var list contains triples of: 
+ *  - flow var access
+ *  - stream contribution (inStream() expression or var access)
+ *  - is this connection to an outside connector (bool literal)
+ */
+FExInStream : FBuiltInFunctionCall ::= Default:FExp Eps:FExp Var:FExp*;
 
 /**
  * getInstanceName built-in function.

commit 30ff8be05d10d34b372afd9f4db4d90bce11b5e7

Fixed problem with arrays and stream connectors that was introduced in r8105. (#4617)
git-svn-id: https://svn.jmodelica.org/trunk@8107 7ae51c65-13d5-4921-9d80-6511ddd1419c

Compiler/ModelicaFlatTree/src/jastadd/Arrays.jrag
@@ -295,7 +295,11 @@ aspect Arrays {
 	 * True if any operand is scalar.
 	 */
 	syn boolean FMulExp.isElementWise() = (getLeft().ndims() * getRight().ndims() == 0);
-	
+
+    eq FExInStream.ndims() = expDefiningSize().ndims();
+
+    syn FExp FExInStream.expDefiningSize() = (numVars() > 0) ? streamExp(0) : getDefault();
+
 	/**
 	 * Get array dimensions.
 	 * 
@@ -1000,6 +1004,8 @@ aspect Arrays {
     eq FSpatialDistExp.size()           = vectorizedSize();
     
     eq FStringExp.size()                = getValue().size();
+
+    eq FExInStream.size()               = expDefiningSize().size();
 
     syn Size FBuiltInFunctionCall.vectorizedSize() {
         for (FExp e : myArgs())
@@ -2172,7 +2178,22 @@ aspect Arrays {
         }
         return arr;
     }
-    
+
+    eq FExInStream.getArray() {
+        if (!isArray()) {
+            return super.getArray();
+        }
+        Array arr = new Array(indices());
+        for (Index i : arr.indices()) {
+            FExInStream cell = new FExInStream(getDefault().getArray().get(i).treeCopy(), getEps().treeCopy(), new List<FExp>());
+            for (FExp e : getVars()) {
+                Index j = (e.ndims() < i.ndims()) ? i.partIndex(0, e.ndims()) : i;
+                cell.addVarNoTransform(e.getArray().get(j).treeCopy());
+            }
+            arr.set(i, cell);
+        }
+        return arr;
+    }
 
 	/**
 	 * Add a copy of this expression to arr at position i. 

Compiler/ModelicaFrontEnd/src/test/StreamTests.mo
@@ -1618,6 +1618,238 @@ end StreamTests.StreamWithConst3;
 end StreamWithConst3;
 
 
+model StreamWithArrays1
+    connector C
+        Real p;
+        flow Real f;
+        stream Real s[0];
+    end C;
+
+    model A
+        C c1, c2, c3;
+        Real x1[0], x2[0], x3[0];
+    equation
+        connect(c1, c2);
+        connect(c2, c3);
+        x1 = inStream(c1.s);
+        x2 = inStream(c2.s);
+        x3 = inStream(c3.s);
+    end A;
+    
+    model B
+        C c(p = 1);
+    end B;
+
+    A a;
+    B b;
+equation
+    connect(a.c1, b.c);
+
+    annotation(__JModelica(UnitTesting(tests={
+        TransformCanonicalTestCase(
+            name="StreamWithArrays1",
+            description="Test handling of outer stream connectors with arrays",
+            flatModel="
+fclass StreamTests.StreamWithArrays1
+ constant Real a.c1.p = 1;
+ constant Real a.c1.f = -0.0;
+ constant Real a.c2.f = 0;
+ constant Real a.c3.f = 0;
+ constant Real a.c2.p = 1;
+ constant Real a.c3.p = 1;
+ constant Real b.c.p = 1;
+ constant Real b.c.f = -0.0;
+end StreamTests.StreamWithArrays1;
+")})));
+end StreamWithArrays1;
+
+
+model StreamWithArrays2
+    connector C
+        Real p;
+        flow Real f;
+        stream Real s[2];
+    end C;
+
+    model A
+        C c1, c2, c3;
+        Real x1[2], x2[2], x3[2];
+    equation
+        connect(c1, c2);
+        connect(c2, c3);
+        x1 = inStream(c1.s);
+        x2 = inStream(c2.s);
+        x3 = inStream(c3.s);
+    end A;
+    
+    model B
+        C c(p = 1, s = {1,2});
+    end B;
+
+    A a;
+    B b;
+equation
+    connect(a.c1, b.c);
+
+    annotation(__JModelica(UnitTesting(tests={
+        TransformCanonicalTestCase(
+            name="StreamWithArrays2",
+            description="Test handling of outer stream connectors with arrays",
+            flatModel="
+fclass StreamTests.StreamWithArrays2
+ constant Real a.c1.p = 1;
+ constant Real a.c1.f = -0.0;
+ Real a.c1.s[1];
+ Real a.c1.s[2];
+ constant Real a.c2.f = 0;
+ constant Real a.c3.f = 0;
+ constant Real a.x1[1] = 1;
+ constant Real a.x1[2] = 2;
+ Real a.x2[1];
+ Real a.x2[2];
+ Real a.x3[1];
+ Real a.x3[2];
+ constant Real a.c2.p = 1;
+ constant Real a.c3.p = 1;
+ constant Real b.c.p = 1;
+ constant Real b.c.f = -0.0;
+ constant Real b.c.s[1] = 1;
+ constant Real b.c.s[2] = 2;
+equation
+ a.c1.s[1] = 0.0;
+ a.c1.s[2] = 0.0;
+ a.x2[1] = 0.0;
+ a.x2[2] = 0.0;
+ a.x3[1] = 0.0;
+ a.x3[2] = 0.0;
+end StreamTests.StreamWithArrays2;
+")})));
+end StreamWithArrays2;
+
+
+model StreamWithArrays3
+    connector C
+        Real p;
+        flow Real f;
+        stream Real s[2];
+    end C;
+
+    model A
+        C c1[2], c2[2], c3[2];
+    equation
+        connect(c1, c2);
+        connect(c2, c3);
+    end A;
+    
+    model B
+        C c[2](p = {1,2}, s = {{1,2},{3,4}});
+    end B;
+
+    A a[2];
+    B b[2];
+equation
+    connect(a.c1, b.c);
+
+    annotation(__JModelica(UnitTesting(tests={
+        TransformCanonicalTestCase(
+            name="StreamWithArrays3",
+            description="Test handling of outer stream connectors with arrays",
+            flatModel="
+fclass StreamTests.StreamWithArrays3
+ constant Real a[1].c1[1].p = 1;
+ constant Real a[1].c1[1].f = -0.0;
+ Real a[1].c1[1].s[1];
+ Real a[1].c1[1].s[2];
+ constant Real a[1].c1[2].p = 2;
+ constant Real a[1].c1[2].f = -0.0;
+ Real a[1].c1[2].s[1];
+ Real a[1].c1[2].s[2];
+ constant Real a[1].c2[1].f = 0;
+ Real a[1].c2[1].s[1];
+ Real a[1].c2[1].s[2];
+ constant Real a[1].c2[2].f = 0;
+ Real a[1].c2[2].s[1];
+ Real a[1].c2[2].s[2];
+ constant Real a[1].c3[1].f = 0;
+ Real a[1].c3[1].s[1];
+ Real a[1].c3[1].s[2];
+ constant Real a[1].c3[2].f = 0;
+ Real a[1].c3[2].s[1];
+ Real a[1].c3[2].s[2];
+ constant Real a[2].c1[1].p = 1;
+ constant Real a[2].c1[1].f = -0.0;
+ Real a[2].c1[1].s[1];
+ Real a[2].c1[1].s[2];
+ constant Real a[2].c1[2].p = 2;
+ constant Real a[2].c1[2].f = -0.0;
+ Real a[2].c1[2].s[1];
+ Real a[2].c1[2].s[2];
+ constant Real a[2].c2[1].f = 0;
+ Real a[2].c2[1].s[1];
+ Real a[2].c2[1].s[2];
+ constant Real a[2].c2[2].f = 0;
+ Real a[2].c2[2].s[1];
+ Real a[2].c2[2].s[2];
+ constant Real a[2].c3[1].f = 0;
+ Real a[2].c3[1].s[1];
+ Real a[2].c3[1].s[2];
+ constant Real a[2].c3[2].f = 0;
+ Real a[2].c3[2].s[1];
+ Real a[2].c3[2].s[2];
+ constant Real b[1].c[1].s[1] = 1;
+ constant Real b[1].c[1].s[2] = 2;
+ constant Real b[1].c[2].s[1] = 3;
+ constant Real b[1].c[2].s[2] = 4;
+ constant Real b[2].c[1].s[1] = 1;
+ constant Real b[2].c[1].s[2] = 2;
+ constant Real b[2].c[2].s[1] = 3;
+ constant Real b[2].c[2].s[2] = 4;
+ constant Real a[1].c2[1].p = 1;
+ constant Real a[1].c2[2].p = 2;
+ constant Real a[1].c3[1].p = 1;
+ constant Real a[1].c3[2].p = 2;
+ constant Real a[2].c2[1].p = 1;
+ constant Real a[2].c2[2].p = 2;
+ constant Real a[2].c3[1].p = 1;
+ constant Real a[2].c3[2].p = 2;
+ constant Real b[1].c[1].p = 1;
+ constant Real b[1].c[1].f = -0.0;
+ constant Real b[1].c[2].p = 2;
+ constant Real b[1].c[2].f = -0.0;
+ constant Real b[2].c[1].p = 1;
+ constant Real b[2].c[1].f = -0.0;
+ constant Real b[2].c[2].p = 2;
+ constant Real b[2].c[2].f = -0.0;
+equation
+ a[1].c1[1].s[1] = 0.0;
+ a[1].c1[1].s[2] = 0.0;
+ a[1].c2[1].s[1] = 0.0;
+ a[1].c2[1].s[2] = 0.0;
+ a[1].c3[1].s[1] = 0.0;
+ a[1].c3[1].s[2] = 0.0;
+ a[1].c1[2].s[1] = 0.0;
+ a[1].c1[2].s[2] = 0.0;
+ a[1].c2[2].s[1] = 0.0;
+ a[1].c2[2].s[2] = 0.0;
+ a[1].c3[2].s[1] = 0.0;
+ a[1].c3[2].s[2] = 0.0;
+ a[2].c1[1].s[1] = 0.0;
+ a[2].c1[1].s[2] = 0.0;
+ a[2].c2[1].s[1] = 0.0;
+ a[2].c2[1].s[2] = 0.0;
+ a[2].c3[1].s[1] = 0.0;
+ a[2].c3[1].s[2] = 0.0;
+ a[2].c1[2].s[1] = 0.0;
+ a[2].c1[2].s[2] = 0.0;
+ a[2].c2[2].s[1] = 0.0;
+ a[2].c2[2].s[2] = 0.0;
+ a[2].c3[2].s[1] = 0.0;
+ a[2].c3[2].s[2] = 0.0;
+end StreamTests.StreamWithArrays3;
+")})));
+end StreamWithArrays3;
+
+
 // TODO: Add error tests (e.g. stream connector without flow)
 
 end StreamTests;