svn2github/JModelica_dot_org

commit fe3f032b94d45441b65e971388c24451c5f414c9

Improved scalabilty for algorithms with a lot of scalars. Ticket #4992.
git-svn-id: https://svn.jmodelica.org/trunk@9034 7ae51c65-13d5-4921-9d80-6511ddd1419c

Compiler/ModelicaMiddleEnd/src/jastadd/structural/BLT.jrag
@@ -1337,6 +1337,28 @@ aspect BLT{
             }
 
         }
+
+        /**
+         * Tries to merge the provided block with this one if they are of the
+         * same type. True is returned if the merge was successful, false
+         * otherwise.
+         */
+        public final boolean merge(AbstractEquationBlock block) {
+            if (getClass().equals(block.getClass()) && do_merge(block)) {
+                return true;
+            }
+            return false;
+        }
+        
+        /**
+         * Internal method for merging, subclasses should override and try to
+         * merge. Return true on successful merge. No type check is needed, it
+         * has already been checked.
+         */
+        protected boolean do_merge(AbstractEquationBlock block) {
+            return false;
+        }
+
         
     }
 
@@ -1346,8 +1368,8 @@ aspect BLT{
      */
     public class EquationBlock extends AbstractEquationBlock {
 
-        private Collection<SimpleEquationBlock> unsolvedBlocks = new ArrayList<SimpleEquationBlock>();
-        private Collection<AbstractEquationBlock> solvedBlocks = new ArrayList<AbstractEquationBlock>();
+        private java.util.List<SimpleEquationBlock> unsolvedBlocks = new ArrayList<SimpleEquationBlock>();
+        private java.util.List<AbstractEquationBlock> solvedBlocks = new ArrayList<AbstractEquationBlock>();
         private Collection<SimpleEquationBlock> localRealBlocks_cache = null;
         private Collection<SimpleEquationBlock> localSolvedBlocks_cache = null;
         private Collection<SimpleEquationBlock> localDiscreteRealBlocks_cache = null;
@@ -1362,7 +1384,13 @@ aspect BLT{
         
         public EquationBlock(EquationBlockFactory.BlockProducer producer, BlockDependency blockDependency, Collection<? extends AbstractEquationBlock> solvedBlocks, Collection<SimpleEquationBlock> unsolvedBlocks) {
             super(producer, blockDependency);
-            this.solvedBlocks.addAll(solvedBlocks);
+            AbstractEquationBlock lastSolved = null;
+            for (AbstractEquationBlock block : solvedBlocks) {
+                if (lastSolved == null || !lastSolved.merge(block)) {
+                    this.solvedBlocks.add(block);
+                    lastSolved = block;
+                }
+            }
             this.unsolvedBlocks.addAll(unsolvedBlocks);
         }
         
@@ -1380,7 +1408,9 @@ aspect BLT{
          * @param block Block to add to the list of solved blocks
          */
         public void addSolvedBlock(AbstractEquationBlock block) {
-            solvedBlocks.add(block);
+            if (solvedBlocks.size() == 0 || !solvedBlocks.get(solvedBlocks.size() - 1).merge(block)) {
+                solvedBlocks.add(block);
+            }
             resetCache();
         }
 
@@ -2390,6 +2420,20 @@ aspect BLT{
         @Override
         public Collection<FAbstractEquation> localDifferentiatedEquations() {
             return new ArrayList<FAbstractEquation>();
+        }
+
+        @Override
+        protected boolean do_merge(AbstractEquationBlock block) {
+            AlgorithmBlock other = (AlgorithmBlock) block;
+            if (equation() == other.equation() &&
+                    isReal() == other.isReal() &&
+                    isDiscreteReal() == other.isDiscreteReal()) {
+                activeVars.addAll(other.activeVars);
+                assignedIndices.addAll(other.assignedIndices);
+                return true;
+            } else {
+                return false;
+            }
         }
     }
 

Compiler/ModelicaCBackEnd/test/modelica/CCodeGenTests.mo
@@ -12008,11 +12008,6 @@ static int dae_block_0(jmi_t* jmi, jmi_real_t* x, jmi_real_t* residual, int eval
     int ef = 0;
     JMI_ARR(STATREAL, jmi_ad_var_t, jmi_array_t, tmp_1, 2, 1)
     JMI_ARR(STATREAL, jmi_ad_var_t, jmi_array_t, tmp_2, 2, 1)
-    JMI_DEF(REA, tmp_3)
-    JMI_DEF(REA, tmp_4)
-    JMI_DEF(REA, tmp_5)
-    JMI_DEF(REA, tmp_6)
-    JMI_DEF(REA, tmp_7)
     if (evaluation_mode == JMI_BLOCK_VALUE_REFERENCE) {
         x[0] = 2;
     } else if (evaluation_mode == JMI_BLOCK_SOLVED_REAL_VALUE_REFERENCE) {
@@ -12028,10 +12023,6 @@ static int dae_block_0(jmi_t* jmi, jmi_real_t* x, jmi_real_t* residual, int eval
         if ((evaluation_mode & JMI_BLOCK_EVALUATE_NON_REALS) == 0) {
             _y_2 = x[0];
         }
-        tmp_4 = _x_1_0;
-        tmp_5 = _temp_1_1_3;
-        tmp_6 = _temp_1_2_4;
-        tmp_7 = _x_2_1;
         _x_1_0 = _y_2;
         JMI_ARRAY_INIT_1(STATREAL, jmi_ad_var_t, jmi_array_t, tmp_1, 2, 1, 2)
         JMI_ARRAY_INIT_1(STATREAL, jmi_ad_var_t, jmi_array_t, tmp_2, 2, 1, 2)
@@ -12041,22 +12032,6 @@ static int dae_block_0(jmi_t* jmi, jmi_real_t* x, jmi_real_t* residual, int eval
         memcpy(&_temp_1_1_3, &jmi_array_val_1(tmp_1, 1), 2 * sizeof(jmi_real_t));
         _x_1_0 = _temp_1_1_3;
         _x_2_1 = _temp_1_2_4;
-        tmp_3 = _x_1_0;
-        _x_1_0 = tmp_4;
-        tmp_4 = tmp_3;
-        tmp_3 = _temp_1_1_3;
-        _temp_1_1_3 = tmp_5;
-        tmp_5 = tmp_3;
-        tmp_3 = _temp_1_2_4;
-        _temp_1_2_4 = tmp_6;
-        tmp_6 = tmp_3;
-        tmp_3 = _x_2_1;
-        _x_2_1 = tmp_7;
-        tmp_7 = tmp_3;
-        _temp_1_1_3 = (tmp_5);
-        _temp_1_2_4 = (tmp_6);
-        _x_2_1 = (tmp_7);
-        _x_1_0 = (tmp_4);
         if (evaluation_mode & JMI_BLOCK_EVALUATE) {
             (*res)[0] = _x_1_0 + 1 - (_y_2);
         }
@@ -12763,9 +12738,6 @@ static int dae_block_0(jmi_t* jmi, jmi_real_t* x, jmi_real_t* residual, int eval
     /***** Block: 1 *****/
     jmi_real_t** res = &residual;
     int ef = 0;
-	JMI_DEF(REA, tmp_1)
-	JMI_DEF(REA, tmp_2)
-	JMI_DEF(REA, tmp_3)
     if (evaluation_mode == JMI_BLOCK_VALUE_REFERENCE) {
         x[0] = 0;
     } else if (evaluation_mode == JMI_BLOCK_SOLVED_REAL_VALUE_REFERENCE) {
@@ -12779,18 +12751,8 @@ static int dae_block_0(jmi_t* jmi, jmi_real_t* x, jmi_real_t* residual, int eval
         if ((evaluation_mode & JMI_BLOCK_EVALUATE_NON_REALS) == 0) {
             _x_0 = x[0];
         }
-        tmp_2 = _y_1;
-        tmp_3 = _z_2;
         _y_1 = _x_0 * 2 + 2;
         _z_2 = _y_1 + _x_0;
-        tmp_1 = _y_1;
-        _y_1 = tmp_2;
-        tmp_2 = tmp_1;
-        tmp_1 = _z_2;
-        _z_2 = tmp_3;
-        tmp_3 = tmp_1;
-        _z_2 = (tmp_3);
-        _y_1 = (tmp_2);
         if (evaluation_mode & JMI_BLOCK_EVALUATE) {
             (*res)[0] = 3 - (_y_1 + _x_0 + _z_2);
         }
@@ -12803,9 +12765,6 @@ static int dae_init_block_0(jmi_t* jmi, jmi_real_t* x, jmi_real_t* residual, int
     /***** Init block: 1 *****/
     jmi_real_t** res = &residual;
     int ef = 0;
-    JMI_DEF(REA, tmp_1)
-    JMI_DEF(REA, tmp_2)
-    JMI_DEF(REA, tmp_3)
     if (evaluation_mode == JMI_BLOCK_VALUE_REFERENCE) {
         x[0] = 0;
     } else if (evaluation_mode == JMI_BLOCK_SOLVED_REAL_VALUE_REFERENCE) {
@@ -12819,18 +12778,8 @@ static int dae_init_block_0(jmi_t* jmi, jmi_real_t* x, jmi_real_t* residual, int
         if ((evaluation_mode & JMI_BLOCK_EVALUATE_NON_REALS) == 0) {
             _x_0 = x[0];
         }
-        tmp_2 = _y_1;
-        tmp_3 = _z_2;
         _y_1 = _x_0 * 2 + 2;
         _z_2 = _y_1 + _x_0;
-        tmp_1 = _y_1;
-        _y_1 = tmp_2;
-        tmp_2 = tmp_1;
-        tmp_1 = _z_2;
-        _z_2 = tmp_3;
-        tmp_3 = tmp_1;
-        _z_2 = (tmp_3);
-        _y_1 = (tmp_2);
         if (evaluation_mode & JMI_BLOCK_EVALUATE) {
             (*res)[0] = 3 - (_y_1 + _x_0 + _z_2);
         }
@@ -13888,9 +13837,6 @@ static int dae_block_0(jmi_t* jmi, jmi_real_t* x, jmi_real_t* residual, int eval
     /***** Block: 1 *****/
     jmi_real_t** res = &residual;
     int ef = 0;
-    JMI_DEF(REA, tmp_1)
-    JMI_DEF(REA, tmp_2)
-    JMI_DEF(REA, tmp_3)
     if (evaluation_mode == JMI_BLOCK_SOLVED_REAL_VALUE_REFERENCE) {
         x[0] = 3;
         x[1] = 4;
@@ -13950,8 +13896,6 @@ static int dae_block_0(jmi_t* jmi, jmi_real_t* x, jmi_real_t* residual, int eval
             }
             _temp_1_3 = _sw(0);
         }
-        tmp_2 = _a_1;
-        tmp_3 = _b_2;
         _a_1 = pre_a_1;
         _b_2 = pre_b_2;
         if (LOG_EXP_AND(_temp_1_3, LOG_EXP_NOT(pre_temp_1_3))) {
@@ -13961,19 +13905,12 @@ static int dae_block_0(jmi_t* jmi, jmi_real_t* x, jmi_real_t* residual, int eval
         } else if (LOG_EXP_OR(LOG_EXP_OR(LOG_EXP_OR(LOG_EXP_AND(_temp_3_5, LOG_EXP_NOT(pre_temp_3_5)), LOG_EXP_AND(_temp_4_6, LOG_EXP_NOT(pre_temp_4_6))), LOG_EXP_AND(_temp_5_7, LOG_EXP_NOT(pre_temp_5_7))), LOG_EXP_AND(_temp_6_8, LOG_EXP_NOT(pre_temp_6_8)))) {
             _b_2 = _b_2 + 1;
         }
-        tmp_1 = _a_1;
-        _a_1 = tmp_2;
-        tmp_2 = tmp_1;
-        tmp_1 = _b_2;
-        _b_2 = tmp_3;
-        tmp_3 = tmp_1;
-        _a_1 = (tmp_2);
-        _b_2 = (tmp_3);
         if (evaluation_mode & JMI_BLOCK_EVALUATE) {
         }
     }
     return ef;
 }
+
 
 ")})));
 end Algorithm16;
@@ -14036,7 +13973,6 @@ static int dae_block_0(jmi_t* jmi, jmi_real_t* x, jmi_real_t* residual, int eval
     JMI_DEF(REA, tmp_1)
     JMI_DEF(REA, tmp_2)
     JMI_ARR(STATREAL, jmi_ad_var_t, jmi_array_t, tmp_3, 2, 1)
-    JMI_DEF(REA, tmp_4)
     if (evaluation_mode == JMI_BLOCK_VALUE_REFERENCE) {
         x[0] = 2;
     } else if (evaluation_mode == JMI_BLOCK_SOLVED_REAL_VALUE_REFERENCE) {
@@ -14050,22 +13986,12 @@ static int dae_block_0(jmi_t* jmi, jmi_real_t* x, jmi_real_t* residual, int eval
         if ((evaluation_mode & JMI_BLOCK_EVALUATE_NON_REALS) == 0) {
             _c_2 = x[0];
         }
-        tmp_1 = _a_0;
-        tmp_2 = _b_1;
         JMI_ARRAY_INIT_1(STATREAL, jmi_ad_var_t, jmi_array_t, tmp_3, 2, 1, 2)
         jmi_array_ref_1(tmp_3, 1) = - _c_2;
         jmi_array_ref_1(tmp_3, 2) = _time;
         func_CCodeGenTests_Algorithm18_F_def0(tmp_3, &tmp_1, &tmp_2);
         _a_0 = (tmp_1);
         _b_1 = (tmp_2);
-        tmp_4 = _a_0;
-        _a_0 = tmp_1;
-        tmp_1 = tmp_4;
-        tmp_4 = _b_1;
-        _b_1 = tmp_2;
-        tmp_2 = tmp_4;
-        _b_1 = (tmp_2);
-        _a_0 = (tmp_1);
         if (evaluation_mode & JMI_BLOCK_EVALUATE) {
             (*res)[0] = _a_0 * _b_1 - (_c_2);
         }

Compiler/ModelicaMiddleEnd/src/jastadd/structural/Graphs.jadd
@@ -295,9 +295,6 @@ public abstract class AbstractBiPGraph<E extends AbstractEq<E, V>, V extends Abs
             if (n_existing != 0 && (n_existing - n_existing_meta) != n_eq) {
                 throw new IllegalArgumentException("Equation already exist in BiPGraph but with different number of scalars, previously " + n_existing + ", now " + n_eq + ", equation:\n" + e);
             }
-            if (n_eq > 1000) {
-                throw new org.jmodelica.util.exceptions.InternalCompilerError("Sorry, this equation is too big, it has " + n_eq + " continuous/discrete scalars. Equation in question:\n" + e);
-            }
             if (n_existing == 0) {
                 groupMembers = new ArrayList<E>();
                 Enumerator subEnumerator = enumerator;

Compiler/ModelicaMiddleEnd/test/modelica/TearingTests.mo
@@ -571,11 +571,9 @@ Torn equations:
   algorithm
     x := x + y + z;
     y := y - x + z;
+
     Assigned variables: y
-  algorithm
-    x := x + y + z;
-    y := y - x + z;
-    Assigned variables: x
+                        x
 
 Residual equations:
   z = x + y

commit 34694d6a5567535cb3f904ac0b586284ec0473af

Fixed regression introduced in r9034. Jacobian calculation no longer assumes that SimpleEquationBlocks are scalar. Ticket #4992.
git-svn-id: https://svn.jmodelica.org/trunk@9046 7ae51c65-13d5-4921-9d80-6511ddd1419c

Compiler/ModelicaMiddleEnd/src/jastadd/structural/BLT.jrag
@@ -877,6 +877,13 @@ aspect BLT{
         public abstract Collection<FAbstractEquation> realEquations();
 
         /**
+         * Returns all real blocks in this block.
+         * 
+         * @return Collection<SimpleEquationBlock> all real blocks
+         */
+        public abstract Collection<SimpleEquationBlock> localRealBlocks();
+
+        /**
          * Returns all local equations in this block that are of type real
          * 
          * Local equations are equations that are computed directly in this
@@ -1117,7 +1124,7 @@ aspect BLT{
         public boolean computeJacobian(Set<FAbstractEquation> activeRealEquations) {
             if (!canComputeJacobian())
                 return false;
-            jacobian = computeJacobian(localRealEquations(), localRealVariables(), activeRealEquations);
+            jacobian = computeJacobian(localRealBlocks(), activeRealEquations);
             variability = computeVariability(jacobian);
             return true;
         }
@@ -1183,28 +1190,30 @@ aspect BLT{
          */
         public abstract void errorCheck(FClass fclass);
 
-        protected final static FExp[][] computeJacobian(Collection<FAbstractEquation> eqns, Collection<FVariable> vars, Set<FAbstractEquation> activeRealEquations) {
-            int numEqns = 0;
-            for (FAbstractEquation eqn : eqns) {
-                if (!eqn.isMetaEquation()) {
-                    numEqns++;
+        protected final static FExp[][] computeJacobian(Collection<SimpleEquationBlock> blocks, Set<FAbstractEquation> activeRealEquations) {
+            
+            Collection<FAbstractEquation> eqns = new ArrayList<FAbstractEquation>();
+            Collection<FVariable> vars = new ArrayList<FVariable>();
+            
+            for (SimpleEquationBlock block : blocks) {
+                Collection<FVariable> blockVars = block.localRealVariables();
+                if (blockVars.size() == 0 || block.isMeta()) {
+                    continue;
                 }
-            }
-            int numVars = vars.size();
-            if (numEqns != numVars) {
-                ASTNode.log.debug("Oh boy, something went wrong when constructing this block!");
-                ASTNode.log.debug("Variables of intereset:");
-                for (FVariable var : vars) {
-                    ASTNode.log.debug(var);
-                }
-                ASTNode.log.debug("Equations of intereset:");
-                for (FAbstractEquation eqn : eqns) {
-                    ASTNode.log.debug(eqn);
-                }
+                vars.addAll(blockVars);
                 
-                throw new IllegalArgumentException("Incorrect arguments to AbstractEquationBlock.computeJacobian(). The number of equations is " + numEqns + " and number of variables is " + numVars + "!");
+                Collection<FAbstractEquation> blockEqns = block.localRealEquations();
+                // There should be exactly one equation in this list
+                if (blockEqns.size() != 1) {
+                    throw new IllegalArgumentException("Incorrect arguments to AbstractEquationBlock.computeJacobian(). There are " + blockVars.size() + " local real variables but " + blockEqns.size() + " local real equations in sub-block:" + block);
+                }
+                // Let's add it n number of times, one for each variable
+                for (int i = 0; i < blockVars.size(); i++) {
+                    eqns.addAll(blockEqns);
+                }
             }
-            FExp[][] jacobian = new FExp[numEqns][numVars];
+            
+            FExp[][] jacobian = new FExp[eqns.size()][vars.size()];
             int row = 0;
             int col = 0;
             Map<FVariable, Integer> columnMap = new HashMap<FVariable, Integer>();
@@ -1212,9 +1221,6 @@ aspect BLT{
                 columnMap.put(fv, col++);
             
             for (FAbstractEquation fe : eqns) {
-                if (fe.isMetaEquation()) {
-                    continue;
-                }
                 if (!activeRealEquations.contains(fe)) {
                     jacobian[row][row] = new FRealLitExp(1);
                     jacobian[row][row].parent = fe; // TODO: This is ugly, yes!
@@ -1442,11 +1448,7 @@ aspect BLT{
             return solvedBlocks;
         }
 
-        /**
-         * Returns all real blocks in this block.
-         * 
-         * @return Collection<SimpleEquationBlock> all real blocks
-         */
+        @Override
         public Collection<SimpleEquationBlock> localRealBlocks() {
             if (localRealBlocks_cache == null) {
                 localRealBlocks_cache = new ArrayList<SimpleEquationBlock>();
@@ -1844,6 +1846,11 @@ aspect BLT{
         }
 
         @Override
+        public Collection<SimpleEquationBlock> localRealBlocks() {
+            return Collections.emptyList();
+        }
+
+        @Override
         public Collection<FVariable> realVariables() {
             Set<FVariable> res = new LinkedHashSet<FVariable>();
             for (T blt : subBLTs)
@@ -2050,6 +2057,15 @@ aspect BLT{
         @Override
         public Collection<FAbstractEquation> realEquations() {
             return localRealEquations();
+        }
+
+        @Override
+        public Collection<SimpleEquationBlock> localRealBlocks() {
+            if (isReal()) {
+                return Collections.singletonList(this);
+            } else {
+                return Collections.emptyList();
+            }
         }
 
         @Override

Compiler/ModelicaMiddleEnd/test/modelica/TransformCanonicalTests.mo
@@ -7665,6 +7665,67 @@ pre(b) := false
 ")})));
 end LinearBlockTest2;
 
+model LinearBlockTest3
+    Real a,b,c,d,e;
+equation
+    a = time + d * 2;
+    e = a * 2 - d;
+    d = c * 2 + a;
+algorithm
+    when e > 0 then
+        b := pre(d) + 1;
+        c := b / 2;
+    end when;
+
+    annotation(__JModelica(UnitTesting(tests={
+        FClassMethodTestCase(
+            name="LinearBlockTest3",
+            description="Test generation of linear blocks with non-scalar algorithm block in the solved part",
+            methodName="printDAEBLT",
+            methodResult="
+--- Torn mixed linear system (Block 1) of 1 iteration variables and 4 solved variables ---
+Coefficient variability: constant
+Torn variables:
+  a
+  e
+  b
+  c
+
+Iteration variables:
+  d
+
+Solved discrete variables:
+  temp_1
+
+Torn equations:
+  a := time + d * 2
+  e := a * 2 + (- d)
+  algorithm
+    if temp_1 and not pre(temp_1) then
+      b := pre(d) + 1;
+      c := b / 2;
+    end if;
+
+    Assigned variables: b
+                        c
+
+Continuous residual equations:
+  d = c * 2 + a
+    Iteration variables: d
+
+Discrete equations:
+  temp_1 := e > 0
+
+Jacobian:
+  |1.0, 0.0, 0.0, 0.0, -2|
+  |0.0, 1.0, 0.0, 0.0, 0.0|
+  |0.0, 0.0, 1.0, 0.0, 0.0|
+  |0.0, 0.0, 0.0, 1.0, 0.0|
+  |-1.0, 0.0, 0.0, -2, 1.0|
+-------------------------------
+")})));
+end LinearBlockTest3;
+
 
 model Sample1
     Real x;